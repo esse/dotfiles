6b2accdaab1fc7ffac983b2ef9ae25b5857afb59
Piotr Szmielew <p.szmielew@ava.waw.pl>
2015-12-03 11:38:46 +0100

    osxdefaults

---
+++ osxdefaults.sh
@@ -0,0 +1,8 @@
+# Save screenshots to the desktop
+defaults write com.apple.screencapture location -string "$HOME/Desktop/scr"
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
